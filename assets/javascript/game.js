ec9e7044e97c98dea005925420e32c8337b14350
Snowbird <jakounter@yahoo.com>
2018-06-02 13:35:50 -0500

    beginjs

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+var characters = [
+    {
+      name: "Luke",
+      faction: "Jedi",
+      health: 1,
+      attack: 1,
+      counterAttack: 1,
+      beginImgUrl: "",
+      attackImgUrl: "",
+    },
+    {
+      name: "Obi Wan",
+      faction: "Jedi",
+      health: 1,
+      attack: 1,
+      counterAttack: 1,
+      beginImgUrl: "",
+      attackImgUrl: "",
+    },
+    {
+      name: "Darth Vader",
+      faction: "Sith",
+      health: 1,
+      attack: 1,
+      counterAttack: 1,
+      beginImgUrl: "",
+      attackImgUrl: "",
+    },
+    {
+      name: "Kylo Ren",
+      faction: "Sith",
+      health: 1,
+      attack: 1,
+      counterAttack: 1,
+      beginImgUrl: "",
+      attackImgUrl: "",
+    }
+  ];
