609117648dacedb41ecd9546a027a7188aa75928
Snowbird <jakounter@yahoo.com>
2018-06-02 21:01:57 -0500

    errorfixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -44,10 +44,10 @@ var characters = [
 $(document).ready(function(){
     $(".hide1").hide();
     // for (var i = 0; i < characters.length; i++) {
-        $("#char1a")=(characters[0].name);
-        $("#char2a")=(characters[1].name);
-        $("#char3a")=(characters[2].name);
-        $("#char4a")=(characters[3].name);
+        $("#char1a")==(characters[0].name);
+        $("#char2a")==(characters[1].name);
+        $("#char3a")==(characters[2].name);
+        $("#char4a")==(characters[3].name);
 
 })
 
