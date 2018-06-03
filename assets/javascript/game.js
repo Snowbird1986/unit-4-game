3e6136b26bcc32d85c88eed4df6fae05d7679bca
Snowbird <jakounter@yahoo.com>
2018-06-02 20:48:40 -0500

    linkjquer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,10 +38,16 @@ var characters = [
     }
   ];
 
-window.onload = function startgame(){
-    $(".hide1").hide();
-  }
+// window.onload = function startgame(){
+//     $(".hide1").hide();
+//   }
 $(document).ready(function(){
     $(".hide1").hide();
+    // for (var i = 0; i < characters.length; i++) {
+        $("#char1a")=(characters[0].name);
+        $("#char2a")=(characters[1].name);
+        $("#char3a")=(characters[2].name);
+        $("#char4a")=(characters[3].name);
+
 })
 
